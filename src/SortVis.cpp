9e3c7eb1d70ce6a329b9d0e0364497be01f1b968
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2021-07-30 20:59:41 +0200

    generator poc

--- src/SortVis.cpp
+++ src/SortVis.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <memory>
+#include <numeric>
+#include <random>
+#include <vector>
 
 #include <cppcoro/generator.hpp>
 
@@ -20,39 +24,138 @@ struct Sortable
 		Write
 	} accessState = AccessState::None;
 
-	bool operator<(const Sortable& other)
+	bool operator<(const Sortable& other) const noexcept
 	{
 		return value < other.value;
 	}
 
-	Sortable& operator=(int value)
+	Sortable& operator=(int other) noexcept
 	{
-		this->value = value;
+		value = other;
 		return *this;
 	}
 };
 
-cppcoro::generator<int> sorter(int data)
+struct SortableVector
 {
-	int i = 0;
-	while(true)
+	std::vector<Sortable> data;
+
+	explicit SortableVector(std::vector<Sortable> data) : data{std::move(data)}
 	{
-		co_yield i + data;
-		++i;
 	}
+
+	bool less(size_t lhs, size_t rhs) noexcept
+	{
+		data[lhs].accessState = Sortable::AccessState::Read;
+		data[rhs].accessState = Sortable::AccessState::Read;
+		return data[lhs] < data[rhs];
+	}
+
+	void swap(size_t lhs, size_t rhs) noexcept
+	{
+		data[lhs].accessState = Sortable::AccessState::Write;
+		data[rhs].accessState = Sortable::AccessState::Write;
+		std::swap(data[lhs], data[rhs]);
+	}
+
+	[[nodiscard]] size_t size() const noexcept
+	{
+		return data.size();
+	}
+
+	std::vector<Sortable>::iterator begin() noexcept
+	{
+		return data.begin();
+	}
+
+	std::vector<Sortable>::iterator end() noexcept
+	{
+		return data.end();
+	}
+};
+
+constexpr int USELESS_MAGIC_VALUE = 0b01001110100101000101010010110011;
+
+cppcoro::generator<const int> bubble(std::shared_ptr<SortableVector> data)
+{
+	auto& vec = *data;
+	bool changed = false;
+	size_t n = vec.size() - 1;
+	do
+	{
+		changed = false;
+		for(size_t i = 0; i < n; ++i)
+		{
+			bool isLess = vec.less(i + 1, i);
+			co_yield USELESS_MAGIC_VALUE;
+
+			if(isLess)
+			{
+				vec.swap(i + 1, i);
+				co_yield USELESS_MAGIC_VALUE;
+
+				changed = true;
+			}
+		}
+		--n;
+	} while(changed);
 }
 
-constexpr int MAX_NUMS = 10;
+std::ostream& operator<<(std::ostream& os, const std::vector<Sortable>& dat)
+{
+	os << '[';
+	for(const Sortable& elm : dat)
+	{
+		os << ' ' << elm.value;
+	}
+	os << ' ' << ']';
+	return os;
+}
+
+struct GeneratorStorage
+{
+	std::shared_ptr<SortableVector> data;
+	cppcoro::generator<const int> gen;
+	cppcoro::generator<const int>::iterator it;
+	cppcoro::detail::generator_sentinel end;
+
+	GeneratorStorage(std::shared_ptr<SortableVector> datavec,
+	    cppcoro::generator<const int> (*generator)(std::shared_ptr<SortableVector>)) :
+	    data{std::move(datavec)},
+	    gen{generator(data)}, it{gen.begin()}, end{gen.end()}
+	{
+	}
+
+	[[nodiscard]] bool generationLeft() const noexcept
+	{
+		return it != end;
+	}
+};
+
+constexpr size_t TEN = 10;
 
 int main()
 {
-	int offset = 0;
-	std::cin >> offset;
-	cppcoro::generator<int> inf = sorter(offset);
-	auto it = inf.begin();
-	for(int i = 0; i < MAX_NUMS; ++i)
+	std::vector<Sortable> data(TEN);
+	std::iota(data.begin(), data.end(), 0);
+	std::mt19937_64 rng(std::random_device{}());
+	std::shuffle(data.begin(), data.end(), rng);
+
+	std::vector<GeneratorStorage> generators;
+	generators.emplace_back(std::make_shared<SortableVector>(data), bubble);
+
+	bool generationleft = true;
+	while(generationleft)
 	{
-		std::cout << *it << ' ';
-		++it;
+		generationleft = false;
+		for(GeneratorStorage& storage : generators)
+		{
+			if(storage.generationLeft())
+			{
+				std::cout << storage.data->data << std::endl;
+				++storage.it;
+				generationleft = true;
+			}
+		}
 	}
 }
